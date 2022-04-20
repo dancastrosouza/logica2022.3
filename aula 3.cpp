24f4a741d96dff84cfea1569f4509afde65a2ca8
dancastrosouza <104075718+dancastrosouza@users.noreply.github.com>
2022-04-20 12:01:56 -0300

    Add files via upload

---
+++ aula 3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main () {
+  //variaveis
+  string nome;
+  
+  string a = "Danilo";
+  string b = " Alencastro";
+
+ cout << (a + b); 
+
+ return 0;
+
+}
+
